f79a0b31c2ed16f4ba25e46ee4e4d68493f0c775
Vikas Saxena <vikas.saxena47@protonmail.com>
2020-12-28 17:46:07 -0500

    Edit dataframe column names.

--- get_stats.py
+++ get_stats.py
@@ -5,43 +5,24 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-row = ['Date',
-       'Week',
-       'Tm',
-       '@',
-       'Opp',
-       'Result',
-       'GS',
-       'Cmp',
-       'Att',
-       'Cmp%',
-       'PYards',
-       'PTD',
-       'Int',
-       'Rate',
-       'Sacks',
-       ' Y_A',
-       'AY_A',
-       'R_Att',
-       'RYards',
-       'R_Y_A',
-       'R_TD',
-       'R_TD2',
-       'Fmb',
-       'FL',
-       'FR',
-       'FR_Yds',
-       'OSnaps',
-       'OSnap_Per',
-       'DSnaps',
-       'DSnaps_Per',
-       'STSnaps',
-       'STSnaps_Per']
+def get_stats(pfr_url):
 
-url = 'https://www.pro-football-reference.com/players/H/HerbJu00.htm'
+    qb_stats = pd.read_html(pfr_url, attrs= {'id': 'stats'}, skiprows=0)
 
-x = pd.read_html(url, attrs= {'id': 'stats'}, skiprows=1)
+    qb_stats = qb_stats[0].drop(qb_stats[0].iloc[:, 35:104], axis=1)
 
-qb_stats = x[0].drop(x[0].iloc[:,35:104], axis=1)
+    qb_stats.rename(columns={'Unnamed: 0_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 1_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 2_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 3_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 4_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 5_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 6_level_0': 'General'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 22_level_0': 'Rush_TD_2'},level=0, inplace=True)
+    qb_stats.rename(columns={'Unnamed: 3_level_1': 'Home_Road'},level=1, inplace=True)
 
-print(qb_stats)
+    return qb_stats
+
+if __name__ == "__main__":
+    test_url =  'https://www.pro-football-reference.com/players/H/HerbJu00.htm'
+    print(get_stats(test_url))
